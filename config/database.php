e7d2f462b4a86800800815fb90bebf404cb8743c
Paulo Amosse Cuambe <pamossecuambe@gmail.com>
2021-02-25 15:21:17 +0200

    add credit DB connection

--- config/database.php
+++ config/database.php
@@ -83,6 +83,26 @@ return [
             ]) : [],
         ],
 
+        'credit' => [
+            'driver' => 'mysql',
+            'url' => env('EVS_DATABASE_URL'),
+            'host' => env('EVS_DB_HOST', '127.0.0.1'),
+            'port' => env('EVS_DB_PORT', '3306'),
+            'database' => env('CREDIT_DB_DATABASE', 'forge'),
+            'username' => env('EVS_DB_USERNAME', 'forge'),
+            'password' => env('EVS_DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
